e7bd0dc554010585f72c47aa447410f7c36d6bf3
Grace Ghansah <graceghansah2000@gmail.com>
2026-02-02 17:13:33 +0000

    update: created the landing page

--- cinematic/src/pages/index.tsx
+++ cinematic/src/pages/index.tsx
@@ -1,11 +1,11 @@
-const Home = () =>{
-  return(
-  <>
-  
-  <h1>Welcome to Cinematic!!!</h1>
-  <p>Your very own movie recommendation site.</p>
-  
-  </>
-  )
-}
+const Home = () => {
+  return (
+    <>
+      <div className="bg-black text-white min-h-screen flex flex-col justify-center items-center">
+        <h1 className="font-bold text-[64px] mb-8 text-shadow-white-lg">Movies picked just for you</h1>
+        <button className="bg-white text-black py-2 px-4 rounded-[10rem] w-112.25 text-[40px] font-semibold cursor items-center">Explore</button>
+      </div>
+    </>
+  );
+};
 export default Home;
