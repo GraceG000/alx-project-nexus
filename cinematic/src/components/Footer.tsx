3daf415d154002dc2b1ebcde32418ae767672cc2
Grace Ghansah <graceghansah2000@gmail.com>
2026-02-02 17:49:55 +0000

    update: added a link to the dashboard

---
+++ cinematic/src/components/Footer.tsx
@@ -0,0 +1,8 @@
+const Footer: React.FC = () => {
+    return(
+        <>
+           <p className="text-white text-center w-full m-0">© 2026 Cinematic. All rights reserved.</p>
+        </>
+    )
+}
+export default Footer;
