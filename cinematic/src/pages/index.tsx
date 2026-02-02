4c9ff0877aa135cae78e2d8479437dcb96bbacaa
Grace Ghansah <graceghansah2000@gmail.com>
2026-02-02 17:51:52 +0000

    update: removed the footer from the landing page

--- cinematic/src/pages/index.tsx
+++ cinematic/src/pages/index.tsx
@@ -4,18 +4,17 @@ import Link from "next/link";
 const Home = () => {
   return (
     <div className="bg-black m-0 p-0 overflow-hidden">
-       <div className="bg-black text-white min-h-screen flex flex-col justify-center items-center">
-      <h1 className="font-bold text-[64px] mb-8 text-shadow-white-lg">Movies picked just for you</h1>
-      <Link href="/dashboard">
-        <button className="bg-white text-black py-2 px-4 rounded-[10rem] w-112.25 text-[40px] font-semibold cursor-pointer border-none ">Explore</button>
-      </Link>
+      <div className="bg-black text-white min-h-screen flex flex-col justify-center items-center">
+        <h1 className="font-bold text-[64px] mb-7.5 text-shadow-white-lg">
+          Movies picked just for you
+        </h1>
+        <Link href="/dashboard">
+          <button className="bg-white text-black py-2 px-4 rounded-[10rem] w-112.25 text-[40px] font-semibold cursor-pointer border-none ">
+            Explore
+          </button>
+        </Link>
+      </div>
     </div>
-
-    <div className="m-0 position-fixed bottom-0 left-0 w-full bg-black">
-      <Footer />
-    </div>
-    </div>
-
   );
 };
 export default Home;
